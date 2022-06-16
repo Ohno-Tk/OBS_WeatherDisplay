c6d4151b10c0e255ebef9875dd19d16927fe7a57
Ohno <51406176+Ohno-Tk@users.noreply.github.com>
2022-06-17 00:08:26 +0900

    [add]主要都市追加

---
+++ js/Osaka.js
@@ -0,0 +1,18 @@
+// 大阪(270000)の予報を取得
+let url_osaka = "https://www.jma.go.jp/bosai/forecast/data/forecast/270000.json";
+
+fetch(url_osaka)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(weather) {
+        console.log("大阪:");
+        console.log(weather);
+
+        let area = weather[0].timeSeries[0].areas[0];
+
+       // 画面に書き出す
+        document.getElementById("weather_image_osaka").src = "https://www.jma.go.jp/bosai/forecast/img/"+ area.weatherCodes[1] +".svg";
+        document.getElementById("weather_text_osaka").textContent = area.weathers[1];
+        document.getElementById("weather_degrees_osaka").textContent = "～" + weather[0].timeSeries[2].areas[0].temps[1] + "℃";
+    });
